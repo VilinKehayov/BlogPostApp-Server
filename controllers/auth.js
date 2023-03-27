353221592b3b59a5a549df8c6f093b756e7a2fdd
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-27 08:51:48 +0300

    Started to make register controller..

---
+++ controllers/auth.js
@@ -0,0 +1,10 @@
+import { db } from "../db.js";
+
+export const register = (req, res) => {
+  //check existing user
+
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+  db.query(q, [req.body.email, req.body.username]);
+};
+export const login = (req, res) => {};
+export const logout = (req, res) => {};
