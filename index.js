05852441acaee33304d54b2f9c8eb68774e6a97e
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-27 08:42:03 +0300

    Routes and Controllers started..

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express"
+import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/test",(req,res)=>{
-    res.json("It works")
-})
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
 
 app.listen(8080, ()=>{
     console.log("Connected!")
